a3d22eac3c4e51fc72efe3bc52c879682f534e02
Subhang Mall <subhangmall@Subhangs-MacBook-Pro.local>
2022-09-25 18:49:45 -0700

    fixed capitalizatin

--- robotsinterpret.py
+++ robotsinterpret.py
@@ -1,17 +1,18 @@
 import requests
 
-
 def parseLine(line):
+    
+
     try:
         if line[0] == "#":
             return [-1] # -1 means comment, not useful to progrm
-        elif line[:8] == "Disallow":
+        elif line[:8].lower() == "disallow":
             return [0, line[10:]] # 0 is to show disallow, line[10:] gives path
-        elif line[:5] == "Allow":
+        elif line[:5].lower() == "allow":
             return [1, line[7:]] # 1 means allow, line[7:] gives path
-        elif line[:10] == "User-agent":
+        elif line[:10].lower() == "user-agent":
             return [2, line[12:]]
-        elif line[:7] == "Sitemap":
+        elif line[:7].lower() == "sitemap":
             return [3, line[9:]]
         else:
             return [-1]        
@@ -20,18 +21,12 @@ def parseLine(line):
 
 def interpretRobots(domain):
     try:
-        txt = requests.get(domain + "/robots.txt" if domain[-1] != "/" else domain + "robots.txt").text
+        txt = requests.get(domain + "/robots.txt" if domain[-1] != "/" else domain + "robots.txt").text.splitlines()
     except:
-        print("error")
-        return 1
-
-    txt = txt.splitlines()
+        return [{}, []]
+    print(txt)
 
     # if user-agent: * and disallow:/, then leave
-    isCurrentUserAgent = False
-
-    allowed = {}
-    disAllowed = {}
     sitemaps = {}
 
     total = {}
