8030cf1151e9c41bac0d5b6332ef8e6ca09e2176
Subhang Mall <subhangmall@Subhangs-MacBook-Pro.local>
2022-09-27 10:54:42 -0700

    published

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup, find_packages
 setup (
   name='robotstxtinterpreter',
   version='1.0',
-  license='MIT'
+  license='MIT',
   author='randomsoftwareshack',
   author_email='lolimnotputtingmyactualemail@poopmap.net',
   packages=find_packages('src'),
-  package_dir = {'': 'src'},
+  package_dir = {'': 'robotstxtinterpreter'},
   url = 'https://github.com/randomsoftwareshack/robotstxtinterpreter/',
   keywords='robots.txt',
   install_requires=[
