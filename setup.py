9005c021b0252e9e0e338792c03df916f10b3510
randomsoftwareshack <114312159+randomsoftwareshack@users.noreply.github.com>
2022-09-27 10:33:53 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup (
+  name='robotstxtinterpreter',
+  version='1.0',
+  license='MIT'
+  author='randomsoftwareshack',
+  author_email='lolimnotputtingmyactualemail@poopmap.net',
+  packages=find_packages('src'),
+  package_dir = {'': 'src'},
+  url = 'https://github.com/randomsoftwareshack/robotstxtinterpreter/',
+  keywords='robots.txt',
+  install_requires=[
+    'requests',
+  ], 
+)
