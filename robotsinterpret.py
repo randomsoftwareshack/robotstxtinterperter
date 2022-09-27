9d671f36a8d24d5b3cd1eacff7064dae357ee7fb
Subhang Mall <subhangmall@Subhangs-MacBook-Pro.local>
2022-09-27 10:12:35 -0700

    added comments, removed unnecessary lines

--- robotsinterpret.py
+++ robotsinterpret.py
@@ -11,9 +11,9 @@ def parseLine(line):
         elif line[:5].lower() == "allow":
             return [1, line[7:]] # 1 means allow, line[7:] gives path
         elif line[:10].lower() == "user-agent":
-            return [2, line[12:]]
+            return [2, line[12:]] # 2 means user-agent, line[12:] gives userid
         elif line[:7].lower() == "sitemap":
-            return [3, line[9:]]
+            return [3, line[9:]] # 3 means sitemap, line[9:] gives path
         else:
             return [-1]        
     except:
@@ -25,8 +25,6 @@ def interpretRobots(domain):
     except:
         return [{}, []]
 
-    sitemaps = {}
-
     total = {}
     sitemaps = []
 
@@ -35,21 +33,21 @@ def interpretRobots(domain):
     for i in txt:
         parsedLine = parseLine(i)
 
-        if parsedLine[0] == 3:
+        if parsedLine[0] == 3: # 3 is sitemap, so add to sitemap
             sitemaps.append(parsedLine[1])
 
         if userId != "":
-            if parsedLine[0] == -1:
+            if parsedLine[0] == -1: # -1 is comment/error, so just skip line
                 continue
-            elif parsedLine[0] == 0:
+            elif parsedLine[0] == 0: # 0 means disAllowed, append to userId disAllowed
                 total[userId]["disAllowed"].append(parsedLine[1])
-            elif parsedLine[0] == 1:
+            elif parsedLine[0] == 1: # 1 means allowed, append to userId allowed
                 total[userId]["allowed"].append(parsedLine[1])
-            elif parsedLine[0] == 2:
+            elif parsedLine[0] == 2: # 2 means userId, so switch userIDs, create new key pair
                 userId = parsedLine[1]
                 total[userId] = {"allowed": [], "disAllowed": []} 
                     
-        elif parsedLine[0] == 2:
+        elif parsedLine[0] == 2: # again for userID, if userID isn't registered
             userId = parsedLine[1]
             total[userId] = {"allowed": [], "disAllowed": []}
 
